cc9da8ce8860c670606072f2fe8d2dc458b8c7a6
KevinParkKorea <kevin929292@naver.com>
2018-06-14 15:15:46 +0900

    save to : __result__.crawling

--- collect/crawler.py
+++ collect/crawler.py
@@ -9,6 +9,7 @@ pagename = 'jtbcnews'
 since = '2016-01-01'
 until = '2017-04-31'
 
+
 # url = fb_gen_url(endpoint=END_POINT, service_key=SERVICE_KEY,
 #                   resultCode='',
 #                   resultMsg='',
@@ -27,9 +28,10 @@ def pd_gen_url(endpoint, **param):
     url = '%s?%s&serviceKey=%s' % (endpoint, urlencode(param), SERVICE_KEY)
     return url
 
+
 # ------------------------------------------------------
 def preprocess_tourspot_visitor(item):
-       pass
+    pass
 
 
 def preprocess_foreign_visitor(data):
@@ -64,7 +66,8 @@ def preprocess_foreign_visitor(data):
 
 def crawling_foreign_visitor(country, start_year, end_year):
     result = []
-    for year in range(start_year, end_year+1):
+
+    for year in range(start_year, end_year + 1):
         for month in range(1, 13):
             data = api.pd_fetch_foreign_visitor(country[1], year, month)
             if data is None:
@@ -74,28 +77,36 @@ def crawling_foreign_visitor(country, start_year, end_year):
             result.append(data)
 
     # save data to file
-    print(result)
+    filename = '%s/%s(%s)_crawling_foreign_visitor_%s_%s.json' % (
+        RESULT_DIRECTORY, country[0], country[1], start_year, end_year)
+
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json_string = json.dumps(result, indent=4, sort_keys=True,
+                                 ensure_ascii=False)  # json str으로 덤프하는 과정 텝을 4정도 주고 솔팅을 해라 모두 아스키코드로 해라
+        outfile.write(json_string)
 
 
-
+if os.path.exists(RESULT_DIRECTORY) is False:  # import될때 실행됨
+    os.makedirs(RESULT_DIRECTORY)  # 첫번째 디렉토리가 없으면 하나의 디렉토리가 생성됨
 
 
 def crawling_tourspot_visitor(district, start_year, end_year):
+    results = []
+    filename = '%s/%s_%s_%s.json' % (RESULT_DIRECTORY, 'crawling_tourspot_visitor', since, until)
 
-       results = []
-       filename = '%s/%s_%s_%s.json' % (RESULT_DIRECTORY,'crawling_tourspot_visitor' , since, until)
+    for i in range(start_year, end_year):
+        for j in range(1, 12):
+            for items in pd_fetch_tourspot_visitor():
+                for item in items:  # 10개의 개별 data를 전처리해주기 위한 과정
+                    preprocess_tourspot_visitor(item)
 
-       for i in range(start_year, end_year):
-              for j in range(1, 12):
-                     for items in pd_fetch_tourspot_visitor():
-                            for item in items:  # 10개의 개별 data를 전처리해주기 위한 과정
-                                   preprocess_tourspot_visitor(item)
+                results += items  # 전처리 된 data가 쌓임
 
-                            results += items  # 전처리 된 data가 쌓임
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json_string = json.dumps(results, indent=4, sort_keys=True,
+                                 ensure_ascii=False)  # json str으로 덤프하는 과정 텝을 4정도 주고 솔팅을 해라 모두 아스키코드로 해라
+        outfile.write(json_string)
 
-       with open(filename, 'w', encoding='utf-8') as outfile:
-              json_string = json.dumps(results, indent=4, sort_keys=True, ensure_ascii=False)  # json str으로 덤프하는 과정 텝을 4정도 주고 솔팅을 해라 모두 아스키코드로 해라
-              outfile.write(json_string)
 
-if os.path.exists(RESULT_DIRECTORY) is False: #import될때 실행됨
-    os.makedirs(RESULT_DIRECTORY)   #첫번째 디렉토리가 없으면 하나의 디렉토리가 생성됨
+if os.path.exists(RESULT_DIRECTORY) is False:  # import될때 실행됨
+    os.makedirs(RESULT_DIRECTORY)  # 첫번째 디렉토리가 없으면 하나의 디렉토리가 생성됨
